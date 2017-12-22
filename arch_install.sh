2b26a8eec8b7aa04e2c029e06b5e4354b0c3de88
Garrett Jansen <gjansen@gjansen-Linode.local>
2017-12-22 15:42:50 -0500

    Everything seems to work except for oh-my-zsh grab

--- arch_install.sh
+++ arch_install.sh
@@ -239,6 +239,7 @@ btrfs-progs
 zsh
 zsh-syntax-highlighting
 vim
+go
 git
 openssh
 sysstat
@@ -255,19 +256,20 @@ PKG_LIST_EOF
 
 # install pacaur
 # https://github.com/rmarquis/pacaur
+# Switching to yay
 echo "~ Setting up pacaur \$now"
-pacman -S --noconfirm expac yajl
-if [ ! -n "$(pacman -Qs cower)" ]; then
-  echo "~ Building cower"
-  curl -sL -o /tmp/PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
-  su "$USERNAME" --login -s /bin/bash -c 'cd /tmp && makepkg PKGBUILD --skippgpcheck --needed --noconfirm'
-  pacman -U /tmp/cower*.pkg.tar.xz --noconfirm
-fi
-if [ ! -n "$(pacman -Qs pacaur)" ]; then
-  echo "~ Building pacaur"
-  curl -sL -o /tmp/PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur
+#pacman -S --noconfirm expac yajl
+#if [ ! -n "$(pacman -Qs cower)" ]; then
+#  echo "~ Building cower"
+#  curl -sL -o /tmp/PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
+#  su "$USERNAME" --login -s /bin/bash -c 'cd /tmp && makepkg PKGBUILD --skippgpcheck --needed --noconfirm'
+#  pacman -U /tmp/cower*.pkg.tar.xz --noconfirm
+#fi
+if [ ! -n "$(pacman -Qs yay)" ]; then
+  echo "~ Building yay"
+  curl -sL -o /tmp/PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay
   su "$USERNAME" --login -s /bin/bash -c 'cd /tmp && makepkg PKGBUILD --needed --noconfirm'
-  pacman -U /tmp/pacaur*.pkg.tar.xz --noconfirm
+  pacman -U /tmp/yay*.pkg.tar.xz --noconfirm
 fi
 
 # basic SSH config/lockdown
@@ -284,109 +286,25 @@ echo "$SSHKEY" >> "/home/$USERNAME/.ssh/authorized_keys"
 chmod -R 700 "/home/$USERNAME/.ssh"
 chmod 600 "/home/$USERNAME/.ssh/authorized_keys"
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "installing oh-my-zsh"
+if [ ! -n "$(pacman -Qs oh-my-zsh-git)" ]; then
+  echo "~ Building oh-my-zsh"
+  curl -sL -o /tmp/PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=oh-my-zsh-git
+  su "$USERNAME" --login -s /bin/bash -c 'cd /tmp && makepkg PKGBUILD --needed --noconfirm'
+  pacman -U /tmp/oh-my-zsh-git*.pkg.tar.xz --noconfirm
+fi
 
 cat > "/home/$USERNAME/.zshrc" << ZSH_CONFIG_EOF
-# If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:/usr/local/bin:$PATH
-
-# Path to your oh-my-zsh installation.
-  export ZSH=/home/garrett/.oh-my-zsh
-
-# Set name of the theme to load. Optionally, if you set this to "random"
-# it'll load a random theme each time that oh-my-zsh is loaded.
-# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
+export ZSH=/home/garrett/.oh-my-zsh
 ZSH_THEME="clean"
-
-# Set list of themes to load
-# Setting this variable when ZSH_THEME=random
-# cause zsh load theme from this variable instead of
-# looking in ~/.oh-my-zsh/themes/
-# An empty array have no effect
-# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
-
-# Uncomment the following line to use case-sensitive completion.
-# CASE_SENSITIVE="true"
-
-# Uncomment the following line to use hyphen-insensitive completion. Case
-# sensitive completion must be off. _ and - will be interchangeable.
-# HYPHEN_INSENSITIVE="true"
-
-# Uncomment the following line to disable bi-weekly auto-update checks.
-# DISABLE_AUTO_UPDATE="true"
-
-# Uncomment the following line to change how often to auto-update (in days).
-# export UPDATE_ZSH_DAYS=13
-
-# Uncomment the following line to disable colors in ls.
-# DISABLE_LS_COLORS="true"
-
-# Uncomment the following line to disable auto-setting terminal title.
-# DISABLE_AUTO_TITLE="true"
-
-# Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
-
-# Uncomment the following line to display red dots whilst waiting for completion.
-# COMPLETION_WAITING_DOTS="true"
-
-# Uncomment the following line if you want to disable marking untracked files
-# under VCS as dirty. This makes repository status check for large repositories
-# much, much faster.
-# DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
-# The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# HIST_STAMPS="mm/dd/yyyy"
-
-# Would you like to use another custom folder than $ZSH/custom?
-# ZSH_CUSTOM=/path/to/new-custom-folder
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
 plugins=(
   git
 )
-
 source $ZSH/oh-my-zsh.sh
-
-# User configuration
-
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
-
-# ssh
-# export SSH_KEY_PATH="~/.ssh/rsa_id"
-
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
-
 autoload -Uz promptinit
 promptinit
 source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 autoload predict-on
-
 export EDITOR=vim
 eval $(thefuck --alias)
 
@@ -410,13 +328,6 @@ nameserver 149.112.112.112
 nameserver 2620:fe::fe
 
 RESOLV_EOF
-
-echo "~ Updating Pacman repos"
-echo >> /etc/pacman.conf << PACMAN_EOF
-[archstrike]
-Server = https://mirror.archstrike.org/$arch/$repo
-
-PACMAN_EOF
 
 exit
 ARCH_STRAP_EOF
